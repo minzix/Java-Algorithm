d3928785e58cb625045c8d618c084e4386fc44c8
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-04 16:23:18 +0900

    add : 1-1. 문자 찾기

---
+++ chapter01/question01/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int solution(String input, char character) {
+        // 대소문자 구분 없이 실행하기
+        input = input.toLowerCase();
+        character = Character.toLowerCase(character);
+
+        // 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 세는 용도의 변수
+        int count = 0;
+        for (char c : input.toCharArray()) { // 문자열 -> 문자 배열로 변환 후 반복문으로 순회
+            if (c == character) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        // 입력 받기
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        char character = scanner.nextLine().charAt(0);
+
+        // solution 함수 호출해서 답 출력
+        System.out.println(solution(input, character));
+        scanner.close();
+    }
+}

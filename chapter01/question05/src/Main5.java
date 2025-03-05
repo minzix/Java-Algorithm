2984bffa73df342dfe9aa75a92225a92829eed04
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-05 15:13:50 +0900

    Add : 1-5. 특정 문자 뒤집기

--- chapter01/question05/src/Main5.java
+++ chapter01/question05/src/Main5.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Main5 {
+    public static String solution(String str) {
+        char[] arr = str.toCharArray(); // 문자열을 문자 배열로 변환
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            // 왼쪽 문자가 알파벳이 아닐 경우 건너뛰기
+            if (!Character.isLetter(arr[left])) {
+                left++;
+                continue;
+            }
+
+            // 오른쪽 문자가 알파벳이 아닐 경우 건너뛰기
+            if (!Character.isLetter(arr[right])) {
+                right--;
+                continue;
+            }
+
+            // 두 문자가 모두 알파벳이면 Swap
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // 인덱스 이동
+            left++;
+            right--;
+        }
+
+        return new String(arr); // 문자 배열을 다시 문자열로 변환
+    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
 
+        System.out.println(solution(str));
     }
 }

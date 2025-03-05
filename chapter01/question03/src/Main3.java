b56ef43b542d41b3e624f68e5dba4c4b37fbe032
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-05 14:11:51 +0900

    Fix : 커밋 오류 해결

--- chapter01/question03/src/Main3.java
+++ chapter01/question03/src/Main3.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main3 {
+    public static String solution(String str) {
+        // 특정 구분자로 문자열을 나누기 위해 split 사용하기 (결과값은 배열)
+        String[] words = str.split(" ");
+        String result = "";
+        for (String word : words) {
+            if (result.length() < word.length()) {
+                result = word;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        System.out.println(solution(str));
+    }
 }
--- chapter01/question04/src/Main4.java
+++ chapter01/question04/src/Main4.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main4 {
+    public static String[] solution (int num, String[] words) {
+        String[] answer = new String[num];
+        for (int i = 0; i < num; i++) { // words 배열 순회
+            String word = words[i];
+            int index = word.length();
+            String newWord = "";
+            for (int j = index - 1; j >= 0; j--) {
+                newWord += word.charAt(j);
+            }
+            answer[i] = newWord;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(); // 숫자만 읽고 개행 문자(\n)를 버퍼에 남김.
+        scanner.nextLine();
+        String[] words = new String[num];
+        for (int i = 0; i < num; i++) {
+            words[i] = scanner.nextLine();
+        }
+
+        String[] results = solution(num, words);
+        for (String result : results) {
+            System.out.println(result);
+        }
+
+    }
 }

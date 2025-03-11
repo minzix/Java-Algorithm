1612a74227f05522135c8078426599186753af28
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-11 19:44:39 +0900

    Add : 2-1. 큰 수 출력하기

---
+++ chapter02/question01/src/Main2_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main2_1 {
+    public static StringBuilder solution(int[] A) {
+        StringBuilder answer = new StringBuilder();
+        answer.append(A[0]).append(" ");
+        for (int i = 1; i < A.length; i++) {
+            if (A[i - 1] < A[i]) {
+                answer.append(A[i]).append(" ");
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] A = new int[num];
+        for (int i = 0; i < num; i++) {
+            A[i] = scanner.nextInt();
+        }
+        System.out.println(solution(A));
+    }
+}

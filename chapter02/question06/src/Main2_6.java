e2f3251b0190604bf4e4829cee7b086a3afaa5b0
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-11 21:13:14 +0900

    2-6. 뒤집은 소수

---
+++ chapter02/question06/src/Main2_6.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main2_6 {
+    // 5번 강의 듣고 나서 다시 보기
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
